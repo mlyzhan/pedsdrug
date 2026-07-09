0de482d8e8845a4311c1dfc39f1b1e0647dafd3f
mlyzhan <han.166@qq.com>
2026-07-09 21:15:30 +0800

    最后修改

--- js/app.js
+++ js/app.js
@@ -220,17 +220,19 @@ function nextPage() { var total = Math.ceil(searchResults.length / PAGE_SIZE); i
 function showDetail(idx) {
   var d = searchResults[idx];
   selectedDrug = d;
-  var html = "<div class='detail-title-row'><span class='drug-title'>" + d['药物'] + "</span><span class='drug-tag'>" + d['分类信息'] + "</span></div>";
+  var html = "<div class='drug-title'>" + d['药物'] + "</div>";
   html += "<hr style='margin:10px 0;border:none;border-top:1px solid #eee'>";
 
   if (d['来源'] === '普通药物') {
     html += renderField("目录层级", d['分类信息'], false);
+    html += "<div style='margin-bottom:10px;padding-left:0'><span class='drug-tag'>" + d['分类信息'] + "</span></div>";
     html += renderField("途径", d['途径'], false);
     html += renderField("适应症", formatDrugText(d['适应症']), true);
     html += renderField("儿童用法用量", formatDrugText(d['儿童用法用量']), true);
     html += renderField("注意事项", formatDrugText(d['注意事项']), true);
   } else {
     html += renderField("化学结构 / 代次", d['分类信息'], false);
+    html += "<div style='margin-bottom:10px;padding-left:0'><span class='drug-tag'>" + d['分类信息'] + "</span></div>";
     html += renderField("途径", d['途径'], false);
     html += renderField("抗菌谱", formatDrugText(d['抗菌谱']), true);
     html += renderField("适应症", formatDrugText(d['适应症']), true);
